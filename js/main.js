ec236bcbf17df879faa5abe67b394ce4a77e6b3d
HoangNam2k3 <namhoang6266@gmail.com>
2023-05-31 17:22:52 +0700

    31/5/2023

--- js/main.js
+++ js/main.js
@@ -50,66 +50,45 @@ addActiveNoC("mobile-menu__navbar-wrap", "menu-item-has-children");
 
 // 
 // Add open
-const navToggler = document.getElementsByClassName("nav-toggler")[0]
-const modal = document.getElementsByClassName("offCanvas__wrap")[0]
-const modalContainer = document.getElementsByClassName("mobile-menu-wrap")[0]
-const modalClose = document.getElementsByClassName("close-modal")[0]
+// Add open
+const addOpens = document.querySelectorAll(".addOpen-js");
+const modals = document.querySelectorAll(".modal-js");
+const modalContainers = document.querySelectorAll(".modalContainer-js");
+const modalCloses = document.querySelectorAll(".close-modal");
 
-// ham hien thi modal mua ve (them class open vao modal)
-function addOpen() {
-    modal.classList.add('open')
-}
-//lap qua tung the button va nghe hanh vi click
-navToggler.addEventListener('click',addOpen )
-// Ham an modal mua ve (go class open cua modal)
-function removeOpen(){
-    modal.classList.remove('open')
+// Hien thi va an modal
+function toggleModal(modalElem, containerElem) {
+  modalElem.classList.toggle("open");
+  containerElem.addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
 }
 
-//nghe hanh vi click vao nut button close
-modalClose.addEventListener('click', removeOpen)
+Array.from(addOpens).forEach((navToggler, i) => {
+  const modal = modals[i];
+  const modalContainer = modalContainers[i];
+  const modalClose = modalCloses[i];
 
-modal.addEventListener('click', removeOpen)
-
-modalContainer.addEventListener('click',function (event) {
-    event.stopPropagation()
-})
-
-const modal1 = document.getElementsByClassName("offCanvas__wrap")[1]
-const menuSearch = document.getElementsByClassName("menu__action-search")[0]
-const modalClose1 = document.getElementsByClassName("close-modal")[1]
-
-function addOpen1() {
-    modal1.classList.add('open')
-}
-menuSearch.addEventListener('click',addOpen1 )
-function removeOpen1(){
-    modal1.classList.remove('open')
-}
-modalClose1.addEventListener('click', removeOpen1)
+  // Nghe su kien click tuong ung va goi ham toggleModal
+  navToggler.addEventListener("click", function () {
+    toggleModal(modal, modalContainer);
+  });
+  modalClose.addEventListener("click", function () {
+    toggleModal(modal, modalContainer);
+  });
+  modal.addEventListener("click", function () {
+    toggleModal(modal, modalContainer);
+  });
+});
 
 
-const navToggler2 = document.getElementsByClassName("side-toggle-icon")[0]
-const modal2 = document.getElementsByClassName("offCanvas__wrap")[2]
-const modalContainer2 = document.getElementsByClassName("offCanvas-body")[0]
-const modalClose2 = document.getElementsByClassName("close-modal")[2]
 
-// ham hien thi modal mua ve (them class open vao modal)
-function addOpen2() {
-    modal2.classList.add('open')
-}
-//lap qua tung the button va nghe hanh vi click
-navToggler2.addEventListener('click',addOpen2 )
-// Ham an modal mua ve (go class open cua modal)
-function removeOpen2(){
-    modal2.classList.remove('open')
-}
-
-//nghe hanh vi click vao nut button close
-modalClose2.addEventListener('click', removeOpen2)
-
-modal2.addEventListener('click', removeOpen2)
-
-modalContainer2.addEventListener('click',function (event) {
-    event.stopPropagation()
-})
+function toggleModal1(modalElem, containerElem) {
+    containerElem.addEventListener("click", function (event) {
+      modalElem.classList.add("addOpen");
+      event.stopPropagation();
+    });
+  }
+const x = document.querySelector(".contact-wrap")
+const y = document.querySelector(".contact-js")
+toggleModal1(x, y)
